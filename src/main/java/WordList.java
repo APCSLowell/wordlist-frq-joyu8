d441556b8029a742ecb1d3f1c6376d4accbb8b14
joyu8 <142926471+joyu8@users.noreply.github.com>
2024-04-03 09:32:25 -0700

    Update WordList.java

--- src/main/java/WordList.java
+++ src/main/java/WordList.java
@@ -10,13 +10,21 @@ public class WordList
 
   public int numWordsOfLength(int len)
   {
-    /* to be implemented in part (a) */
-    
+    int a = 0; 
+    for(int i = 0; i < myList.size(); i++){
+      if(myList.get(i).length() == len)
+          a++;
+    }
+    return a;
   }
 
   public void removeWordsOfLength(int len)
   {
-    /* to be implemented in part (b) */
-
+    for(int i = 0; i< myList.size(); i++){
+    if(myList.get(i).length() == len){
+      myList.remove(myList.get(i));
+      i--;
+    }
   }
+}
 }
